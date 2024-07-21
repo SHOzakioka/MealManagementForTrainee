598e06c3f3e1195737c8527086537b7b6b003e59
SHOzakioka <zakioka.sho@gmail.com>
2024-07-22 05:53:20 +0900

    基礎代謝を計算するServiceクラスの実装

--- src/main/java/jp/co/meal_management/domain/service/BodyMetricsServiceImpl.java
+++ src/main/java/jp/co/meal_management/domain/service/BodyMetricsServiceImpl.java
@@ -2,6 +2,8 @@ package jp.co.meal_management.domain.service;
 
 import org.springframework.stereotype.Service;
 
+import jp.co.meal_management.config.BodyMetricsConstant;
+
 /**
  * @author sho.okazaki <br>
  * 基礎代謝を計算するserviceクラス
@@ -9,65 +11,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BodyMetricsServiceImpl implements BodyMetricsService {
-
-	@Override
-	public double calcurateMar(int sex, double weightKg, double heightCm, int age) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+	
+	private final BodyMetricsConstant bodyMetricsConstant;
+	
+	public BodyMetricsServiceImpl(BodyMetricsConstant bodyMetricsConstant) {
+		this.bodyMetricsConstant = bodyMetricsConstant;
 	}
 
-//	/* 男性 */
-//	private final Integer MEN;
-//	/* 女性 */
-//	private final Integer WOMEN;
-//	/* その他 */
-//	private final Integer OTHER;
-//
-//	/* ハリスベネディクト方程式　男性体重の系数 */
-//	private final double MEN_WEIGHT_KG_CONST;
-//	/* ハリスベネディクト方程式　男性身長の系数 */
-//	private final double MEN_HEIGHT_CM_CONST;
-//	/* ハリスベネディクト方程式　男性年齢の系数 */
-//	private final double MEN_AGE_CONST;
-//	/* ハリスベネディクト方程式　男性定数項 */
-//	private final double MEN_CONST;
-//
-//	/* ハリスベネディクト方程式　女性体重の系数 */
-//	private final double WOMEN_WEIGHT_KG_CONST;
-//	/* ハリスベネディクト方程式　女性身長の系数 */
-//	private final double WOMEN_HEIGHT_CM_CONST;
-//	/* ハリスベネディクト方程式　女性年齢の系数 */
-//	private final double WOMEN_AGE_CONST;
-//	/* ハリスベネディクト方程式　女性定数項 */
-//	private final double WOMEN_CONST;
-//	
-//	public BodyMetricsServiceImpl(@Value("${constant.file.name:Constant}") String constantFileName) {
-//		ResourceBundle rb = ResourceBundle.getBundle(constantFileName);
-//
-//		this.MEN = Integer.parseInt(rb.getString("MEN"));
-//		this.WOMEN = Integer.parseInt(rb.getString("WOMEN"));
-//		this.OTHER = Integer.parseInt(rb.getString("OTHER"));
-//		this.MEN_WEIGHT_KG_CONST = Integer.parseInt(rb.getString("MEN_WEIGHT_KG_CONST"));
-//		this.MEN_HEIGHT_CM_CONST = Integer.parseInt(rb.getString("MEN_HEIGHT_CM_CONST"));
-//		this.MEN_AGE_CONST = Integer.parseInt(rb.getString("MEN_AGE_CONST"));
-//		this.MEN_CONST = Integer.parseInt(rb.getString("MEN_CONST"));
-//		this.WOMEN_WEIGHT_KG_CONST = Integer.parseInt(rb.getString("WOMEN_WEIGHT_KG_CONST"));
-//		this.WOMEN_HEIGHT_CM_CONST = Integer.parseInt(rb.getString("WOMEN_HEIGHT_CM_CONST"));
-//		this.WOMEN_AGE_CONST = Integer.parseInt(rb.getString("WOMEN_AGE_CONST"));
-//		this.WOMEN_CONST = Integer.parseInt(rb.getString("WOMEN_CONST"));
-//	}
-//
-//	/**
-//	 * 計算した基礎代謝(MAR)を返す
-//	 * @return MAR
-//	 */
-//	@Override
-//	public double calcurateMar(int sex, double weightKg, double heightCm, int age) {
-//		if (MEN.equals(sex)) {
-//			return MEN_WEIGHT_KG_CONST * weightKg + MEN_HEIGHT_CM_CONST * heightCm - MEN_AGE_CONST * age + MEN_CONST;
-//		} else {
-//			return WOMEN_WEIGHT_KG_CONST * weightKg + WOMEN_HEIGHT_CM_CONST * heightCm - WOMEN_AGE_CONST * age
-//					+ WOMEN_CONST;
-//		}
-//	}
+	/**
+	 * 計算した基礎代謝(MAR)を返す
+	 * @return MAR
+	 */
+	@Override
+	public double calcurateMar(int sex, double weightKg, double heightCm, int age) {
+		if (bodyMetricsConstant.getMen() == sex) {
+			return bodyMetricsConstant.getMenWeightKgConst() * weightKg
+					+ bodyMetricsConstant.getMenHeightCmConst() * heightCm
+					- bodyMetricsConstant.getMenAgeConst() * age
+					+ bodyMetricsConstant.getMenConst();
+		} else {
+			return bodyMetricsConstant.getWomenWeightKgConst() * weightKg
+					+ bodyMetricsConstant.getWomenHeightCmConst() * heightCm
+					- bodyMetricsConstant.getWomenAgeConst() * age
+					+ bodyMetricsConstant.getWomenConst();
+		}
+	}
 }
--- src/main/java/jp/co/meal_management/use_case/BodyMetricsRecordImpl.java
+++ src/main/java/jp/co/meal_management/use_case/BodyMetricsRecordImpl.java
@@ -8,6 +8,7 @@ import jp.co.meal_management.domain.entity.BodyMetrics;
 import jp.co.meal_management.domain.entity.BodyMetricsId;
 import jp.co.meal_management.domain.repository.BodyMetricsRepository;
 import jp.co.meal_management.domain.repository.CustomRepository;
+import jp.co.meal_management.domain.service.BodyMetricsService;
 
 /**
  * @author sho.okazaki <br>
@@ -15,36 +16,46 @@ import jp.co.meal_management.domain.repository.CustomRepository;
  */
 
 @Component
-public class BodyMetricsSaveRecordImpl implements MealManagementRecord {
+public class BodyMetricsRecordImpl implements MealManagementRecord {
 
 	/* インスタンス変数の定義 */
 	private final CustomRepository customRepository;
-	private final BodyMetricsRepository bodyMetricsRepository; 
-	
-	public BodyMetricsSaveRecordImpl(CustomRepository customRepository, BodyMetricsRepository bodyMetricsRepository) {
+	private final BodyMetricsRepository bodyMetricsRepository;
+	private final BodyMetricsService bodyMetricsService;
+
+	public BodyMetricsRecordImpl(
+			CustomRepository customRepository
+			, BodyMetricsRepository bodyMetricsRepository
+			, BodyMetricsService bodyMetricsService) {
 		this.customRepository = customRepository;
 		this.bodyMetricsRepository = bodyMetricsRepository;
+		this.bodyMetricsService = bodyMetricsService;
 	}
 
 	// インスタンス変数としてuserIdとrecordDateを取得しておきたい
 
 	/**
-	 * 体重の値を受取ってテーブルを更新するメソッドです。
+	 * 体重の値を受取ってテーブルを更新するメソッドです。 <br>
+	 * 体重を更新すると同時に基礎代謝も更新します。
 	 */
 	@Override
 	public void saveWeightEntity(double weightKg) {
 		BodyMetrics bodyMetrics = new BodyMetrics();
-		
+
 		// user_idを設定
 		bodyMetrics.setUserId(005);
 		// 今日の日付を設定
 		bodyMetrics.setRecordDate(new Date());
-		
 		// BodyMetricsIdのuserIdに値を設定
-		BodyMetricsId bodyMetricsId = new BodyMetricsId(bodyMetrics.getUserId(),bodyMetrics.getRecordDate()); 
-		
+		BodyMetricsId bodyMetricsId = new BodyMetricsId(bodyMetrics.getUserId(), bodyMetrics.getRecordDate());
 
+		// 体重を更新
 		customRepository.upsert(bodyMetricsRepository, bodyMetrics, bodyMetricsId, "weightKg", weightKg);
+		
+		// 基礎代謝を計算
+		double mar = bodyMetricsService.calcurateMar(1, weightKg, 172.6, 25);
+		// 基礎代謝を更新
+		customRepository.upsert(bodyMetricsRepository, bodyMetrics, bodyMetricsId, "mar", mar);
 	}
 
 }
